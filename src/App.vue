e96740685f205d51acf04b5371279ca08f8cac58
yangzhiqiong <yangzhiqiong@genomics.cn>
2020-04-28 15:48:55 +0800

    优化菜单栏

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <!--展示给用户，相当于v-->
 <template>
     <div id="app">
-      <img alt="Vue logo" src="./assets/logo.png">
-      <ul>
-        <li><router-link to="/">主页</router-link></li>
-        <li><router-link to="/greeting">问候</router-link></li>
-        <li><router-link to="/login">登录</router-link></li>
-        <li><router-link to="/project_list">项目列表</router-link></li>
-        <li><router-link to="/menu">菜单栏</router-link></li>
-      </ul>
-      <!-- 展示路由 -->
-      <router-view></router-view>
+        <div id="menu">
+            <el-menu
+                    :default-active="activeIndex"
+                    class="el-menu-demo"
+                    mode="horizontal"
+                    @select="handleSelect"
+                    background-color="#545c64"
+                    text-color="#fff"
+                    active-text-color="#ffd04b">
+                <el-menu-item index="1"><router-link to="/">主页</router-link></el-menu-item>
+                <el-menu-item index="2"><router-link to="/greeting">问候</router-link></el-menu-item>
+                <el-menu-item index="3"><router-link to="/login">登录</router-link></el-menu-item>
+                <el-menu-item index="4"><router-link to="/project_list">项目列表</router-link></el-menu-item>
+            </el-menu>
+        </div>
+        <!-- 展示路由 -->
+        <router-view></router-view>
     </div>
 </template>
 
@@ -22,24 +29,40 @@
   // import navMenu from "./components/NavMenu"
 
 export default {
-  name: 'App',
-  components: {
+    name: 'App',
+    components: {
     // HelloWorld,
     // eslint-disable-next-line vue/no-unused-components
     // greeting,
     // login, navMenu,
 
-  }
+    },
+    data() {
+        return {
+            activeIndex: '1',
+        };
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
+    #menu{
+        /*position: fixed;*/
+        float: top;
+        position: relative;
+        top: 0;
+    }
 </style>
--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,6 +1,5 @@
 <template>
     <div id="menu">
-<!--        <div class="line"></div>-->
         <el-menu
                 :default-active="activeIndex"
                 class="el-menu-demo"
@@ -9,21 +8,10 @@
                 background-color="#545c64"
                 text-color="#fff"
                 active-text-color="#ffd04b">
-            <el-menu-item index="1">处理中心</el-menu-item>
-            <el-submenu index="2">
-                <template slot="title">我的工作台</template>
-                <el-menu-item index="2-1">选项1</el-menu-item>
-                <el-menu-item index="2-2">选项2</el-menu-item>
-                <el-menu-item index="2-3">选项3</el-menu-item>
-                <el-submenu index="2-4">
-                    <template slot="title">选项4</template>
-                    <el-menu-item index="2-4-1">选项1</el-menu-item>
-                    <el-menu-item index="2-4-2">选项2</el-menu-item>
-                    <el-menu-item index="2-4-3">选项3</el-menu-item>
-                </el-submenu>
-            </el-submenu>
-            <el-menu-item index="3" disabled>消息中心</el-menu-item>
-            <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+            <el-menu-item index="1"><router-link to="/">主页</router-link></el-menu-item>
+            <el-menu-item index="2"><router-link to="/greeting">问候</router-link></el-menu-item>
+            <el-menu-item index="3"><router-link to="/login">登录</router-link></el-menu-item>
+            <el-menu-item index="4"><router-link to="/project_list">项目列表</router-link></el-menu-item>
         </el-menu>
     </div>
 </template>
